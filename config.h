5487d880665d28528a42405c79ecb7b41f534717
LuiselbotYT <luisdorabae@gmail.com>
2024-03-19 20:08:58 -0700

    added patches. again.

--- config.h
+++ config.h
@@ -3,17 +3,17 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
-static const unsigned int alpha = 0xff;     /* Amount of opacity. 0xff is opaque             */
+static int min_width = 400;                    /* minimum width when centered */
+static const unsigned int alpha = 0xc9;     /* Amount of opacity. 0xff is opaque             */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Inconsolata-SemiExpanded:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#bbbbbb", "#000000" },
+	[SchemeSel] = { "#eeeeee", "#ff00ff" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 
